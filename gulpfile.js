8af6c36e5540220fddacb4db7535abf084171a64
impnguyen <imoney91@me.com>
2018-03-28 23:59:10 +0200

    add gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+
+gulp.task("lint", () => {
+  return gulp
+    .src(["**/*.js", "!node_modules/**"])
+    .pipe(eslint({configFile: '.eslintrc'}))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
